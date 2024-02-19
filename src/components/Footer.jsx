4b3bd62f1b3f8f486bf72d62184151d8b0ebdbf7
2h311 <kcodepal@outlook.com>
2024-02-19 16:36:42 +0100

    done with the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,21 +33,41 @@ const Footer = () => {
           <div className="footer__div footer__div--2">
             <h3>The Company</h3>
             <ul className="footer__links">
-              <span></span>
-              <span></span>
-              <li className="footer__links-list">Home</li>
-              <li className="footer__links-list">Contact us</li>
-              <li className="footer__links-list">About us</li> <br />
-              <li className="footer__links-list">Blog</li>
-              <li className="footer__links-list">Forum</li>
-              <li className="footer__links-list">Products</li>
+              <span className="footer__links-span">
+                <li className="footer__links-list">Home</li>
+                <li className="footer__links-list">Contact us</li>
+                <li className="footer__links-list">About us</li> <br />
+              </span>
+              <span className="footer__links-span">
+                <li className="footer__links-list">Blog</li>
+                <li className="footer__links-list">Forum</li>
+                <li className="footer__links-list">Products</li>
+              </span>
             </ul>
           </div>
-          <div className="footer__div footer__div--3"></div>
+          <div className="footer__div footer__div--3">
+            <h3>Newsletter</h3>
+            <small className="footer__small">
+              Get real time updates from us
+            </small>
+            <div className="footer__newsletter">
+              <input
+                className="footer__newsletter-email"
+                type="text"
+                placeholder="Please Enter Your Valid Email address"
+                name=""
+                id=""
+              />
+              <input
+                className="footer__newsletter-subscribe"
+                type="button"
+                value="Subscribe"
+              />
+            </div>
+          </div>
         </section>
         <section className="footer__down">
-          &copy; eatinghealthy.com &#124; All rights reserved &#124;{" "}
-          {currentYear}
+          &copy; eathealthy.com &#124; All rights reserved &#124; {currentYear}
         </section>
       </div>
     </footer>
