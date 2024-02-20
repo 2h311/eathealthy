331fd769944cf179d57e49d87c31d168bc6318d8
2h311 <kcodepal@outlook.com>
2024-02-20 02:46:37 +0100

    Just create the Gist component for the Gist section of the homepge

---
+++ src/components/Gist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Gist = () => {
+  return <section>Gist</section>;
+};
+
+export default Gist;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Hero from "./Hero";
+import Gist from "./Gist";
 
 const Home = () => {
   return (
     <>
       <Hero />;
+      <Gist />
     </>
   );
 };
