451a2f5bdda26a09f3b976f8b3e9448e58399275
2h311 <kcodepal@outlook.com>
2024-02-20 01:09:17 +0100

    working on the Home componentn

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
       {/* <h1>App</h1> */}
-      <Navbar />
-      <Footer />
+      {/* <Navbar /> */}
+      {/* <Footer /> */}
+      <Home />
     </>
   );
 };
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="home__container"></div>
+    </section>
+  );
+};
+
+export default Home;
