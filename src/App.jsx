6d46b2980da3dd2fe193432cec0b634e7fdcc20f
2h311 <kcodepal@outlook.com>
2024-02-29 09:38:24 +0100

    Home.jsx: made some adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <>
-      {/* <h1>App</h1> */}
       <Home />
     </>
   );
--- src/components/RecentPostVideo.jsx
+++ src/components/RecentPostVideo.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import RecentPostInfo from "./recent-posts/RecentPostInfo";
+
 const RecentPostVideo = () => {
-  return <section className="">RecentPostVideo</section>;
+  const title = "How to make strawberry",
+    description =
+      "Have you ever wanted to learn how to make strawberry milk? I’m going to talk about how to make strawberry milk with three simple steps.",
+      likes = 25,
+      readingTime = "15 mins"
+  return (
+    <section className="recent-post-video">
+      <div className="recent-post-video__inner">
+        <div className="recent-post-video__left">
+          {/* place a video in here */}
+        </div>
+        <div className="recent-post-video__right">
+          {/* place some text in here */}
+          <h3 className="recent-post__heading-text">{title}</h3>
+          <p className="recent-post__description">{description}</p>
+          <RecentPostInfo likes={likes} readingTime={readingTime} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RecentPostVideo;
