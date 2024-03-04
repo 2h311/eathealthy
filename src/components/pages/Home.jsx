a64997dc07db112027d2979e77e6ebcb57ded406
2h311 <kcodepal@outlook.com>
2024-03-04 18:07:17 +0100

    Home.jsx: made some changes around

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+
+import Hero from "../hero/Hero";
+import Gist from "../gist/Gist";
+import Footer from "../footer/Footer";
+import RecentPosts from "../recent-posts/RecentPosts";
+import SendAMessageNow from "../send-a-message-now/SendAMessageNow";
+import RecentPostVideo from "../recent-video/RecentPostVideo";
+import GetMyLatestEbook from "../latest-ebook/GetMyLatestEbook";
+import WhatMakesUsDifferent from "../send-a-message-now/WhatMakesUsDifferent";
+import Ratings from "../ratings/Ratings";
+import Testimonials from "../testimonials/Testimonials";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Hero />
+      <Gist />
+      <RecentPosts />
+      <SendAMessageNow />
+      <RecentPostVideo />
+      <GetMyLatestEbook />
+      <WhatMakesUsDifferent />
+      <Ratings />
+      <Testimonials />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Home;
