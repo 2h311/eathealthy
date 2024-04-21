520ba2dfb880ccc3a5ccec23221e78885cbaed76
2h311 <kcodepal@outlook.com>
2024-04-21 15:31:51 +0100

    Home.jsx: doing some import cleanups

--- src/assets/data/data.jsx
+++ src/assets/data/data.jsx
@@ -1,10 +1,11 @@
-import PexelsAshImage from "../../assets/images/pexels-ash-376464.jpg";
-import PexelsJohnImage from "../../assets/images/pexels-john-finkelstein-1630588.jpg";
-import PexelsMonicoreImage from "../../assets/images/pexels-monicore-1391487.jpg";
-
-import TeamMember1Image from "../../assets/images/team-1.jpg";
-import TeamMember2Image from "../../assets/images/team-2.jpg";
-import TeamMember3Image from "../../assets/images/team-3.jpg";
+import {
+  TeamMember1Image,
+  TeamMember2Image,
+  TeamMember3Image,
+  PexelsAshImage,
+  PexelsJohnImage,
+  PexelsMonicoreImage,
+} from "../images/ImagesInit";
 
 const navbarLinks = [
   { href: "/", text: "home" },
